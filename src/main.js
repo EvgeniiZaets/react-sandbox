2a2232cac352e9f2a795306c55e3740d2375ad57
EvgeniiZaets <evgeniizaets@gmail.com>
2022-09-07 18:32:23 +0300

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log(1 + 1);
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+const root = ReactDom.createRoot(document.querySelector('.app'));
+root.render(
+    <div>
+        <div>Hello, world</div>
+    </div>
+);
