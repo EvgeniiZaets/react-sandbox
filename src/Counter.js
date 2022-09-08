75df2afa5fa9504a70d5662eb55c06e0f3aac858
EvgeniiZaets <evgeniizaets@gmail.com>
2022-09-08 19:52:40 +0300

    component with state

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+export default function({ max }) {
+    let [current, setCurrent] = useState(1);
+
+    function inc() {
+        if (current < max) {
+            setCurrent(current + 1);
+        }
+    }
+
+    return <div>
+        <span>{current}</span>
+        <button type="button" onClick={ inc }>+</button>
+    </div>
+}
+
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 
 import UserCard from "./UserCard";
+import Counter from "./Counter";
 
 const root = ReactDom.createRoot(document.querySelector('.app'));
 root.render(
     <div>
         <div>Hello, world</div>
         <UserCard name="John" text="Hi, there!"/>
+        <Counter max={5}/>
     </div>
 );
