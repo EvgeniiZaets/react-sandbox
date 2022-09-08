756bdb9044dbecd00e552bef2443386fca0aaca3
EvgeniiZaets <evgeniizaets@gmail.com>
2022-09-08 12:36:37 +0300

    component props

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ const root = ReactDom.createRoot(document.querySelector('.app'));
 root.render(
     <div>
         <div>Hello, world</div>
-        <UserCard/>
+        <UserCard name="John" text="Hi, there!"/>
     </div>
 );
 
-function UserCard() {
+function UserCard(props) {
     return <div className="card">
         User Card
+        <div>{props.name} {props.text}</div>
     </div>
 }
