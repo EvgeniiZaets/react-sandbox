fb8f63f14a58927d7cc444d504507351ff52cd14
EvgeniiZaets <evgeniizaets@gmail.com>
2022-09-10 22:45:49 +0300

    products list (unidirectional flow; pass data from child to parent component)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
 import React, { useState } from 'react';
-import Counter from './Counter';
+import MinMax from './MinMax';
 
 export default function App () {
-  const [maxTest, setMaxTest] = useState(10);
-  const setMaxTest5 = () => setMaxTest(5);
+  const [products, setProducts] = useState(productsStub());
+
+  const setCnt = (id, cnt) => {
+    // массив products иммутабелен. если его изменим, то список не перерендерится. поэтому создаем новый массив newProducts
+    // (при этом оперативка не замусоривается т.к. newProducts копирует товары по ссылке)
+
+    // const newProducts = [...products];
+    // const productInd = products.findIndex(product => product.id === id);
+    // const newProduct = { ...products[productInd] };
+    // newProduct.cnt = cnt;
+    // newProducts[productInd] = newProduct;
+    // setProducts(newProducts);
+    setProducts(
+      products.map(product => product.id === id
+        ? ({
+            ...product,
+            cnt
+          })
+        : product)
+    );
+  };
 
   return <div>
-        <Counter min={2} max={maxTest}/>
-        <hr/>
-        <button type="button" onClick={setMaxTest5}>set max 5</button>
-    </div>;
+    <h1>Products List</h1>
+    <table>
+      <tbody>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Cnt</th>
+          <th>Total</th>
+        </tr>
+        { products.map((product, i) => (
+          <tr key={ product.id }>
+            <td>{ i + 1 }</td>
+            <td>{ product.title }</td>
+            <td>{ product.price }</td>
+            <td>
+              <MinMax current={ product.cnt } max={ product.rest } onChange={ cnt => setCnt(product.id, cnt) }/>
+            </td>
+            <td></td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  </div>;
+}
+
+function productsStub () {
+  return [
+    {
+      id: 100,
+      title: 'Iphone 14',
+      price: 1000,
+      rest: 10,
+      cnt: 1
+    },
+    {
+      id: 101,
+      title: 'Pixel 6a',
+      price: 450,
+      rest: 5,
+      cnt: 1
+    },
+    {
+      id: 102,
+      title: 'Nothing Phone',
+      price: 500,
+      rest: 2,
+      cnt: 1
+    }
+  ];
 }
--- src/MinMax.js
+++ src/MinMax.js
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-function Counter ({ min, max }) {
-  const [current, setCurrent] = useState(min);
-
+function MinMax ({ min = 1, max, current, onChange }) {
   function dec () {
     return applyCurrent(current - 1);
   }
@@ -13,23 +11,14 @@ function Counter ({ min, max }) {
   }
 
   function applyCurrent (num) {
-    // if (num <= max && num >= min) {
-    //     setCurrent(num);
-    // }
     const validNum = Math.max(min, Math.min(max, num));
-    setCurrent(validNum);
+    onChange(validNum);
   }
 
   function currentChange (e) {
     const num = parseInt(e.target.value);
     applyCurrent(isNaN(num) ? min : num);
   }
-
-  // make action at the moment of changing values of min/max props
-  // (fix issue when prop "current" is not updated after parent component changes min/max props)
-  useEffect(() => {
-    applyCurrent(current);
-  }, [min, max]);
 
   return <div>
         <button type="button" onClick={ dec }>-</button>
@@ -38,9 +27,11 @@ function Counter ({ min, max }) {
     </div>;
 }
 
-Counter.propTypes = {
-  min: PropTypes.number.isRequired,
-  max: PropTypes.number.isRequired
+MinMax.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number.isRequired,
+  current: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired
 };
 
-export default Counter;
+export default MinMax;
