788f34fe7048e8e06eef9ee464b9081b376a2e8a
EvgeniiZaets <evgeniizaets@gmail.com>
2022-09-12 09:31:28 +0300

    MinMax lazy input (workaround)

--- src/MinMaxLazyState.js
+++ src/MinMaxLazyState.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-// todo
-function MinMax ({ min = 1, max, current, onChange }) {
+function MinMaxLazyState ({ min = 1, max, current, onChange }) {
+  const [inpVal, setInpVal] = useState(current);
+  const onInput = e => setInpVal(e.target.value);
+
+  function onKeyPress (e) {
+    if (e.keyCode === 13) {
+      currentChange(e);
+    }
+  }
+
   function dec () {
     return applyCurrent(current - 1);
   }
@@ -13,6 +21,7 @@ function MinMax ({ min = 1, max, current, onChange }) {
 
   function applyCurrent (num) {
     const validNum = Math.max(min, Math.min(max, num));
+    setInpVal(validNum); // rerender component
     onChange(validNum);
   }
 
@@ -21,18 +30,22 @@ function MinMax ({ min = 1, max, current, onChange }) {
     applyCurrent(isNaN(num) ? min : num);
   }
 
+  useEffect(() => {
+    setInpVal(current); // rerender component
+  }, [current]);
+
   return <div>
     <button type="button" onClick={ dec }>-</button>
-    <input type="text" value={current} onChange={ currentChange }/>
+    <input type="text" value={ inpVal } onChange={ onInput } onBlur={currentChange} onKeyDown={onKeyPress}/>
     <button type="button" onClick={ inc }>+</button>
   </div>;
 }
 
-MinMax.propTypes = {
+MinMaxLazyState.propTypes = {
   min: PropTypes.number,
   max: PropTypes.number.isRequired,
   current: PropTypes.number.isRequired,
   onChange: PropTypes.func.isRequired
 };
 
-export default MinMax;
+export default MinMaxLazyState;
